54bb93dba046f56871bb28316d5b360c48f5b73c
TakumiKato <tako3ch@gmail.com>
2026-03-16 01:06:09 +0900

    Initial Commit

---
+++ nTabula/nTabulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  nTabulaApp.swift
+//  nTabula
+//
+//  Created by TakumiKato on 2026/03/16.
+//
+
+import SwiftUI
+
+@main
+struct nTabulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
